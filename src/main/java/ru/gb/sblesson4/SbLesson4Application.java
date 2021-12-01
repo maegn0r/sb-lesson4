bdae4fe3db242323ac33875c48bafd116c1f1ffc
novitskynick <novitskynick@gmail.com>
2021-12-02 00:20:43 +0300

    Initial commit

---
+++ src/main/java/ru/gb/sblesson4/SbLesson4Application.java
@@ -0,0 +1,13 @@
+package ru.gb.sblesson4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbLesson4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbLesson4Application.class, args);
+    }
+
+}
